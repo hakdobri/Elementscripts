6caace4f1054fdc5900402396abb892b66dd1fb4
Hak <90701868+hakdobri@users.noreply.github.com>
2023-08-30 14:47:22 +0200

    Update deleteads.user.js

--- deleteads.user.js
+++ deleteads.user.js
@@ -14,15 +14,21 @@
     'use strict';
 
     var newButtonHTML = '<a href="https://diurl.site" target="_blank"><button class="NavPanel-Btn">Скрипты</button></a><div style="height: 5px;"></div>';
+    
     var navPanel = document.querySelector('.NavPanel');
+    if (navPanel) {
+        var alreadyAdded = localStorage.getItem('buttonAdded');
 
-    if (navPanel && !navPanel.innerHTML.includes(newButtonHTML)) {
-        var tempContainer = document.createElement('div');
-        tempContainer.innerHTML = newButtonHTML;
-        var exitButton = navPanel.querySelector('a[href="/выход"]');
-        
-        while (tempContainer.firstChild) {
-            navPanel.insertBefore(tempContainer.firstChild, exitButton);
+        if (!alreadyAdded) {
+            var tempContainer = document.createElement('div');
+            tempContainer.innerHTML = newButtonHTML;
+            var exitButton = navPanel.querySelector('a[href="/выход"]');
+            
+            while (tempContainer.firstChild) {
+                navPanel.insertBefore(tempContainer.firstChild, exitButton);
+            }
+
+            localStorage.setItem('buttonAdded', 'true');
         }
     }
     
